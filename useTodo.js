099d0d62b2591ca5f0611801639b5a856b8bc64e
Julian Guerra <julianguerra92@gmail.com>
2022-12-23 15:38:07 -0500

    Creación de custom hooks para retualizar en proyectos de React

---
+++ useTodo.js
@@ -0,0 +1,50 @@
+import { useReducer, useEffect } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [];
+
+const init = () => {
+   return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+   const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+   useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos) || []);
+   }, [todos])
+
+   const handleNewTodo = (todo) => {
+      const action = {
+         type: '[TODO] Add Todo',
+         payload: todo
+      }
+      dispatch(action);
+   }
+
+   const handleDeleteTodo = (id) => {
+      const action = {
+         type: '[TODO] Remove Todo',
+         payload: id
+      }
+      dispatch(action);
+   }
+
+   const handleToggleTodo = (id) => {
+      const action = {
+         type: '[TODO] Toggle Todo',
+         payload: id
+      }
+      dispatch(action);
+   }
+
+   return {
+      todos,
+      handleNewTodo,
+      handleDeleteTodo,
+      handleToggleTodo,
+      pendingTodos: todos.filter(todo => !todo.done).length,
+      countTodos: todos.length,
+   }
+}
